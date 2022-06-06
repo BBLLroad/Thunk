d9a8708bc319b32ccb42e1f70710283e5338d8ac
davisp2 <davisp2@illinois.edu>
2022-06-06 15:30:05 -0500

    Added first class test suite.

---
+++ src/main/test/CaseTest.java
@@ -0,0 +1,27 @@
+package main.test;
+
+import org.testng.annotations.Test;
+
+public class CaseTest {
+    //...TODO::main.test case comments
+
+    @Test
+    public void testAddClause() {
+
+    }
+
+    @Test
+    public void testRemoveClause() {
+
+    }
+
+    @Test
+    public void testAttachClauses() {
+
+    }
+
+    @Test
+    public void testDetachClauses() {
+
+    }
+}
